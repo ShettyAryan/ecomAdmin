403d9df0bb819051a60aa16b41fb34d101c05eb3
Aryan Shetty <shetty.aryan1923@gmail.com>
2025-02-24 23:32:17 +0530

    Nav from scratch p1

---
+++ basicecom-admin/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from './sections/Nav'
+
+const App = () => {
+  return (
+    <main className='relative'>
+        <Nav />
+        
+
+    </main>
+  )
+}
+
+export default App
---
+++ basicecom-admin/src/sections/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import dataicon from '../assets/database-storage.png'
+
+const Nav = () => {
+  return (
+    <div className=' ml-8 mt-5 flex flex-row justify-start gap-8 w-full'>
+        <img src={dataicon} alt="database icon" className='w-10' />
+        
+
+
+    </div>
+  )
+}
+
+export default Nav
